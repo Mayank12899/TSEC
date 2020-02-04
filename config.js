4534325ac13d5e2cc512293089297b233402853e
Mayank12899 <srivastavamayank679@gmail.com>
2020-02-04 16:45:37 +0530

    first commit

---
+++ config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyC1uQPhr2sYzoz4R-yCkDUMl9Jr1g-ru60",
+    authDomain: "web-tech-16df7.firebaseapp.com",
+    databaseURL: "https://web-tech-16df7.firebaseio.com",
+    projectId: "web-tech-16df7",
+    storageBucket: "web-tech-16df7.appspot.com",
+    messagingSenderId: "147239743877",
+    appId: "1:147239743877:web:34fa4ebf3ec96aa46654ec"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ js/config.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDz0lOOZXF9NN_gSntwothTu9BHGSmg5lM",
+  authDomain: "socialcause-anmvqv.firebaseapp.com",
+  databaseURL: "https://socialcause-anmvqv.firebaseio.com",
+  projectId: "socialcause-anmvqv",
+  storageBucket: "socialcause-anmvqv.appspot.com",
+  messagingSenderId: "409349955202",
+  appId: "1:409349955202:web:348c5c20de55a9a40ade9b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
